76a30d049bf5d868f809edf1e743454069a75037
toxac <derocon@gmail.com>
2026-09-07 23:02:39 +0530

    store barrel

---
+++ lib/program/stores/index.ts
@@ -0,0 +1,59 @@
+export {
+  $userProfileStore,
+  setUserProfile,
+  updateUserProfile,
+  clearUserProfile,
+} from './userProfileStore';
+
+export {
+  $userProgressStore,
+  setUserProgress,
+  setNodeProgress,
+  updateNodeProgress,
+  getNodeProgress,
+  clearUserProgress,
+} from './userProgressStore';
+
+export {
+  $userOpportunitiesStore,
+  setUserOpportunities,
+  addUserOpportunity,
+  updateUserOpportunity,
+  clearUserOpportunities,
+} from './userOpportunitiesStore';
+
+export {
+  $userProjectsStore,
+  setUserProjects,
+  addUserProject,
+  updateUserProject,
+  clearUserProjects,
+} from './userProjectsStore';
+
+export {
+  $userContactsStore,
+  setUserContacts,
+  addUserContact,
+  updateUserContact,
+  clearUserContacts,
+} from './userContactsStore';
+
+export {
+  $userCommitmentsStore,
+  setUserCommitments,
+  addUserCommitment,
+  updateUserCommitment,
+} from './userCommitmentsStore';
+
+export {
+  $userTasksStore,
+  setUserTasks,
+  addUserTask,
+  updateUserTask,
+} from './userTasksStore';
+
+export {
+  $userObservationsStore,
+  setUserObservations,
+  addUserObservation,
+} from './userObservationsStore';
