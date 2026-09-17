4c2bd84d9de3fe015a8ca044b6684ffb445f0b01
toxac <derocon@gmail.com>
2026-09-17 10:33:29 +0530

    synec db types

--- types/supabase.ts
+++ types/supabase.ts
@@ -810,6 +810,7 @@ export type Database = {
       user_profile: {
         Row: {
           age_group: string | null
+          avatar_path: string | null
           capabilities: Json
           city: string | null
           constraints: Json
@@ -835,6 +836,7 @@ export type Database = {
         }
         Insert: {
           age_group?: string | null
+          avatar_path?: string | null
           capabilities?: Json
           city?: string | null
           constraints?: Json
@@ -860,6 +862,7 @@ export type Database = {
         }
         Update: {
           age_group?: string | null
+          avatar_path?: string | null
           capabilities?: Json
           city?: string | null
           constraints?: Json
